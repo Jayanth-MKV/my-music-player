d1b72da3951f20eedb05383eccf8eaa84b1d7d35
Jayanth MKV <93820457+Jayanth-MKV@users.noreply.github.com>
2023-09-03 01:03:07 +0530

    files upload

---
+++ data.js
@@ -0,0 +1,202 @@
+export default [
+  {
+    wrapperType: "track",
+    kind: "feature-movie",
+    trackId: 1474205881,
+    artistName: "Anthony Russo & Joe Russo",
+    trackName: "Marvel Studios' Avengers: Endgame",
+    trackCensoredName: "Marvel Studios' Avengers: Endgame",
+    trackViewUrl:
+      "https://itunes.apple.com/in/movie/marvel-studios-avengers-endgame/id1474205881?uo=4",
+    previewUrl:
+      "https://video-ssl.itunes.apple.com/itunes-assets/Video123/v4/b1/51/44/b1514407-0780-fa06-75f8-691f56f0d5d7/mzvf_14454238724794170573.640x354.h264lc.U.p.m4v",
+    artworkUrl30:
+      "https://is4-ssl.mzstatic.com/image/thumb/Video123/v4/9c/80/7b/9c807b2c-33df-59bb-1e00-1ceafaec40f3/pr_source.jpg/30x30bb.jpg",
+    artworkUrl60:
+      "https://is4-ssl.mzstatic.com/image/thumb/Video123/v4/9c/80/7b/9c807b2c-33df-59bb-1e00-1ceafaec40f3/pr_source.jpg/60x60bb.jpg",
+    artworkUrl100:
+      "https://is4-ssl.mzstatic.com/image/thumb/Video123/v4/9c/80/7b/9c807b2c-33df-59bb-1e00-1ceafaec40f3/pr_source.jpg/100x100bb.jpg",
+    collectionPrice: 490,
+    trackPrice: 490,
+    trackRentalPrice: 150,
+    collectionHdPrice: 690,
+    trackHdPrice: 690,
+    trackHdRentalPrice: 150,
+    releaseDate: "2019-04-26T07:00:00Z",
+    collectionExplicitness: "notExplicit",
+    trackExplicitness: "notExplicit",
+    trackTimeMillis: 10870560,
+    country: "IND",
+    currency: "INR",
+    primaryGenreName: "Action & Adventure",
+    contentAdvisoryRating: "U/A 13+",
+    shortDescription:
+      "The fourth installment in the Avengers saga is the culmination of 22 interconnected Marvel films and",
+    longDescription:
+      "The fourth installment in the Avengers saga is the culmination of 22 interconnected Marvel films and the climax of an epic journey. The world’s greatest heroes will finally understand just how fragile our reality is—and the sacrifices that must be made to uphold it—in a story of friendship, teamwork and setting aside differences to overcome an impossible obstacle.",
+  },
+  {
+    wrapperType: "track",
+    kind: "feature-movie",
+    collectionId: 1605254890,
+    trackId: 1598961641,
+    artistName: "Jon Watts",
+    collectionName: "SPIDER-MAN COLLECTION DE 8 FILMS",
+    trackName: "Spider-Man: No Way Home",
+    collectionCensoredName: "SPIDER-MAN COLLECTION DE 8 FILMS",
+    trackCensoredName: "Spider-Man: No Way Home",
+    collectionArtistId: 345346702,
+    collectionArtistViewUrl:
+      "https://itunes.apple.com/in/artist/sony-pictures-entertainment/345346702?uo=4",
+    collectionViewUrl:
+      "https://itunes.apple.com/in/movie/spider-man-no-way-home/id1598961641?uo=4",
+    trackViewUrl:
+      "https://itunes.apple.com/in/movie/spider-man-no-way-home/id1598961641?uo=4",
+    previewUrl:
+      "https://video-ssl.itunes.apple.com/itunes-assets/Video116/v4/7b/46/b9/7b46b91b-53a0-8eb5-0cdb-c84da7411cec/mzvf_2437796329076422647.640x354.h264lc.U.p.m4v",
+    artworkUrl30:
+      "https://is5-ssl.mzstatic.com/image/thumb/Video116/v4/4a/e2/d1/4ae2d1c2-4fe9-5014-4d7d-6b6bff969668/SPE_SPIDERMAN_NO_WAY_HOME_TH_PRE_IN_ARTWORK_EN_2000x3000_3358QV000009A8.lsr/30x30bb.jpg",
+    artworkUrl60:
+      "https://is5-ssl.mzstatic.com/image/thumb/Video116/v4/4a/e2/d1/4ae2d1c2-4fe9-5014-4d7d-6b6bff969668/SPE_SPIDERMAN_NO_WAY_HOME_TH_PRE_IN_ARTWORK_EN_2000x3000_3358QV000009A8.lsr/60x60bb.jpg",
+    artworkUrl100:
+      "https://is5-ssl.mzstatic.com/image/thumb/Video116/v4/4a/e2/d1/4ae2d1c2-4fe9-5014-4d7d-6b6bff969668/SPE_SPIDERMAN_NO_WAY_HOME_TH_PRE_IN_ARTWORK_EN_2000x3000_3358QV000009A8.lsr/100x100bb.jpg",
+    collectionPrice: 290,
+    trackPrice: 290,
+    trackRentalPrice: 100,
+    collectionHdPrice: 290,
+    trackHdPrice: 290,
+    trackHdRentalPrice: 100,
+    releaseDate: "2021-12-16T08:00:00Z",
+    collectionExplicitness: "notExplicit",
+    trackExplicitness: "notExplicit",
+    discCount: 1,
+    discNumber: 1,
+    trackCount: 8,
+    trackNumber: 8,
+    trackTimeMillis: 8890880,
+    country: "IND",
+    currency: "INR",
+    primaryGenreName: "Action & Adventure",
+    contentAdvisoryRating: "U/A 13+",
+    longDescription:
+      "For the first time in the cinematic history of Spider-Man, our friendly neighborhood hero's identity is revealed, bringing his Super Hero responsibilities into conflict with his normal life and putting those he cares about most at risk. When he enlists Doctor Strange’s help to restore his secret, the spell tears a hole in their world, releasing the most powerful villains who’ve ever fought a Spider-Man in any universe. Now, Peter will have to overcome his greatest challenge yet, which will not only forever alter his own future but the future of the Multiverse.",
+  },
+  {
+    wrapperType: "track",
+    kind: "feature-movie",
+    collectionId: 1694351847,
+    trackId: 1688506685,
+    artistName: "Joaquim Dos Santos, Kemp Powers & Justin K. Thompson",
+    collectionName:
+      "Spider-Man: Across The Spider-Verse + Spider-Man: Into The Spider-Verse 2-Movie Collection",
+    trackName: "Spider-Man: Across the Spider-Verse",
+    collectionCensoredName:
+      "Spider-Man: Across The Spider-Verse/Spider-Man: Into The Spider-Verse: 2 Movie Collection",
+    trackCensoredName: "Spider-Man: Across the Spider-Verse",
+    collectionArtistId: 345346702,
+    collectionArtistViewUrl:
+      "https://itunes.apple.com/in/artist/sony-pictures-entertainment/345346702?uo=4",
+    collectionViewUrl:
+      "https://itunes.apple.com/in/movie/spider-man-across-the-spider-verse/id1688506685?uo=4",
+    trackViewUrl:
+      "https://itunes.apple.com/in/movie/spider-man-across-the-spider-verse/id1688506685?uo=4",
+    previewUrl:
+      "https://video-ssl.itunes.apple.com/itunes-assets/Video126/v4/24/ca/41/24ca41ac-d116-30da-b0d4-6c02477b6094/mzvf_3996586131801090583.640x354.h264lc.U.p.m4v",
+    artworkUrl30:
+      "https://is1-ssl.mzstatic.com/image/thumb/Video126/v4/3c/02/1c/3c021c34-851d-bb7f-b426-d62683a4ac42/SPE_SPIDERMAN_ATS_TH_FINAL_ITUNES_WW_ARTWORK_EN_2000x3000_3TUF3R000066S7.lsr/30x30bb.jpg",
+    artworkUrl60:
+      "https://is1-ssl.mzstatic.com/image/thumb/Video126/v4/3c/02/1c/3c021c34-851d-bb7f-b426-d62683a4ac42/SPE_SPIDERMAN_ATS_TH_FINAL_ITUNES_WW_ARTWORK_EN_2000x3000_3TUF3R000066S7.lsr/60x60bb.jpg",
+    artworkUrl100:
+      "https://is1-ssl.mzstatic.com/image/thumb/Video126/v4/3c/02/1c/3c021c34-851d-bb7f-b426-d62683a4ac42/SPE_SPIDERMAN_ATS_TH_FINAL_ITUNES_WW_ARTWORK_EN_2000x3000_3TUF3R000066S7.lsr/100x100bb.jpg",
+    collectionPrice: 490,
+    trackPrice: 350,
+    collectionHdPrice: 690,
+    trackHdPrice: 450,
+    releaseDate: "2023-06-01T07:00:00Z",
+    collectionExplicitness: "notExplicit",
+    trackExplicitness: "notExplicit",
+    trackCount: 2,
+    trackNumber: 2,
+    trackTimeMillis: 8660650,
+    country: "IND",
+    currency: "INR",
+    primaryGenreName: "Action & Adventure",
+    contentAdvisoryRating: "U/A 7+",
+    shortDescription:
+      "Miles Morales returns for the next chapter of the Oscar®-winning Spider-Verse saga, (2018, Best",
+    longDescription:
+      "Miles Morales returns for the next chapter of the Oscar®-winning Spider-Verse saga, (2018, Best Animated Feature Film), Spider-Man™: Across the Spider-Verse. After reuniting with Gwen Stacy, Brooklyn's full-time, friendly neighborhood Spider-Man is catapulted across the Multiverse, where he encounters a team of Spider-People charged with protecting its very existence. But when the heroes clash on how to handle a new threat, Miles finds himself pitted against the other Spiders and must redefine what it means to be a hero so he can save the people he loves most.",
+  },
+  {
+    wrapperType: "track",
+    kind: "feature-movie",
+    trackId: 1473101913,
+    artistName: "Anna Boden & Ryan Fleck",
+    trackName: "Captain Marvel",
+    trackCensoredName: "Captain Marvel",
+    trackViewUrl:
+      "https://itunes.apple.com/in/movie/captain-marvel/id1473101913?uo=4",
+    previewUrl:
+      "https://video-ssl.itunes.apple.com/itunes-assets/Video123/v4/cf/6b/b3/cf6bb368-ab5d-b14e-e0e2-a4e461a6b38e/mzvf_16802837368830938314.640x356.h264lc.U.p.m4v",
+    artworkUrl30:
+      "https://is5-ssl.mzstatic.com/image/thumb/Video123/v4/e6/80/e1/e680e192-1463-1603-7270-c13b7e2e790d/DIS_CPMVL_INDIA_IN_ARTWORK_EN_2000x3000_1STWIE000001WJ.jpg/30x30bb.jpg",
+    artworkUrl60:
+      "https://is5-ssl.mzstatic.com/image/thumb/Video123/v4/e6/80/e1/e680e192-1463-1603-7270-c13b7e2e790d/DIS_CPMVL_INDIA_IN_ARTWORK_EN_2000x3000_1STWIE000001WJ.jpg/60x60bb.jpg",
+    artworkUrl100:
+      "https://is5-ssl.mzstatic.com/image/thumb/Video123/v4/e6/80/e1/e680e192-1463-1603-7270-c13b7e2e790d/DIS_CPMVL_INDIA_IN_ARTWORK_EN_2000x3000_1STWIE000001WJ.jpg/100x100bb.jpg",
+    collectionPrice: 490,
+    trackPrice: 490,
+    trackRentalPrice: 150,
+    collectionHdPrice: 690,
+    trackHdPrice: 690,
+    trackHdRentalPrice: 150,
+    releaseDate: "2019-03-08T08:00:00Z",
+    collectionExplicitness: "notExplicit",
+    trackExplicitness: "notExplicit",
+    trackTimeMillis: 7420640,
+    country: "IND",
+    currency: "INR",
+    primaryGenreName: "Action & Adventure",
+    contentAdvisoryRating: "U/A 13+",
+    shortDescription:
+      "Set in the 1990s, Marvel Studios’ Captain Marvel is an adventure from a previously unseen period in",
+    longDescription:
+      "Set in the 1990s, Marvel Studios’ Captain Marvel is an adventure from a previously unseen period in the history of the Marvel Cinematic Universe that follows the journey of Carol Danvers as she becomes one of the universe’s most powerful heroes. When a galactic war between two alien races reaches Earth, Danvers finds herself and a small cadre of allies at the center of the maelstrom.",
+  },
+  {
+    wrapperType: "track",
+    kind: "feature-movie",
+    trackId: 1454476975,
+    artistName: "Joss Whedon",
+    trackName: "Marvel's Avengers: Age of Ultron",
+    trackCensoredName: "Marvel's Avengers: Age of Ultron",
+    trackViewUrl:
+      "https://itunes.apple.com/in/movie/marvels-avengers-age-of-ultron/id1454476975?uo=4",
+    previewUrl:
+      "https://video-ssl.itunes.apple.com/itunes-assets/Video114/v4/73/27/ca/7327cad0-04b8-7031-1bf2-7b6c214885d2/mzvf_5569791025352946577.640x354.h264lc.U.p.m4v",
+    artworkUrl30:
+      "https://is2-ssl.mzstatic.com/image/thumb/Video114/v4/88/6a/7c/886a7c89-e8ab-0323-800d-730d1c981cfa/DIS_AVENGERS_WW_ARTWORK_EN_2000x3000_1MPG4Q000000DR.jpg/30x30bb.jpg",
+    artworkUrl60:
+      "https://is2-ssl.mzstatic.com/image/thumb/Video114/v4/88/6a/7c/886a7c89-e8ab-0323-800d-730d1c981cfa/DIS_AVENGERS_WW_ARTWORK_EN_2000x3000_1MPG4Q000000DR.jpg/60x60bb.jpg",
+    artworkUrl100:
+      "https://is2-ssl.mzstatic.com/image/thumb/Video114/v4/88/6a/7c/886a7c89-e8ab-0323-800d-730d1c981cfa/DIS_AVENGERS_WW_ARTWORK_EN_2000x3000_1MPG4Q000000DR.jpg/100x100bb.jpg",
+    collectionPrice: 590,
+    trackPrice: 590,
+    trackRentalPrice: 150,
+    collectionHdPrice: 690,
+    trackHdPrice: 690,
+    trackHdRentalPrice: 150,
+    releaseDate: "2015-04-24T07:00:00Z",
+    collectionExplicitness: "notExplicit",
+    trackExplicitness: "notExplicit",
+    trackTimeMillis: 8477375,
+    country: "IND",
+    currency: "INR",
+    primaryGenreName: "Action & Adventure",
+    contentAdvisoryRating: "U/A 13+",
+    shortDescription:
+      "Good intentions wreak havoc when Tony Stark (Robert Downey Jr.) unwittingly creates Ultron, an A.I.",
+    longDescription:
+      "Good intentions wreak havoc when Tony Stark (Robert Downey Jr.) unwittingly creates Ultron, an A.I. monster who vows to achieve “world peace” via mass extinction. Now the Avengers must reassemble to defeat him and save mankind…if they can!",
+  },
+];
